b61af8623868f6a0ddd8da57107a334a0afef5e4
Almer Farand Rafael <farandalmer4@gmail.com>
2025-05-26 12:35:11 +0700

    db

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,16 +1,25 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const db = new Sequelize(
   process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: "mysql", // atau 'postgres', 'sqlite', dsb
-    logging: false,
+    dialect: "mysql",
   }
 );
+
+(async () => {
+  try {
+    await db.authenticate();
+    console.log("Database berhasil terkoneksi.");
+  } catch (error) {
+    console.error("Database gagal terkoneksi:", error);
+  }
+})();
 
 export default db;
