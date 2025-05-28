272b5eea179e28bd784498ec0bae7889a8f233fb
Almer Farand Rafael <farandalmer4@gmail.com>
2025-05-28 20:35:04 +0700

    waw

--- backend/server.js
+++ backend/server.js
@@ -30,16 +30,7 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 
-app.use((req, res, next) => {
-  if (req.method === 'OPTIONS') {
-    res.header("Access-Control-Allow-Origin", "https://fe-040-dot-b-01-450713.uc.r.appspot.com");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-    res.header("Access-Control-Allow-Credentials", "true");
-    return res.sendStatus(200);
-  }
-  next();
-});
+app.options('*', cors());
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
